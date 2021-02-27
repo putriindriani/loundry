bf4bd868e5398baaa5e330265f4cc1bd9b835f50
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-27 15:10:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,233 +1,105 @@
+
 <?php
-session_start();
-if ( !isset($_SESSION['username']) ) {
-    header('location:login.php'); 
-}
-else { 
-    $usr = $_SESSION['username']; 
-}
-require_once('koneksi.php');
-$query = mysql_query("SELECT * FROM pengguna WHERE username = '$usr'");
-$hasil = mysql_fetch_array($query);
-if (empty($hasil['username'])) {
-    header('Location: ./login.php');
-}
-function TanggalIndo($date){
-	$BulanIndo = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
- 
-	$tahun = substr($date, 0, 4);
-	$bulan = substr($date, 5, 2);
-	$tgl   = substr($date, 8, 2);
- 
-	$result = $tgl . " " . $BulanIndo[(int)$bulan-1] . " ". $tahun;		
-	return($result);
-}
-$timezone = "Asia/Jakarta";
-if(function_exists('date_default_timezone_set')) date_default_timezone_set($timezone);
-$date=date('Y-m-d');
-@ini_set('display_errors', 0);
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
-        <meta name="author" content="Coderthemes">
 
-        <link rel="shortcut icon" href="images/favicon_1.ico">
-
-        <title>Aplikasi Laundry</title>
-
-       <link href="css/bootstrap.min.css" rel="stylesheet" />
-        <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-        <link href="assets/ionicon/css/ionicons.min.css" rel="stylesheet" />
-        <link href="css/material-design-iconic-font.min.css" rel="stylesheet">
-        <link href="css/animate.css" rel="stylesheet" />
-        <link href="css/waves-effect.css" rel="stylesheet">
-        <link href="assets/tagsinput/jquery.tagsinput.css" rel="stylesheet" />
-        <link href="assets/toggles/toggles.css" rel="stylesheet" />
-        <link href="assets/timepicker/bootstrap-timepicker.min.css" rel="stylesheet" />
-        <link href="assets/timepicker/bootstrap-datepicker.min.css" rel="stylesheet" />
-        <link href="assets/colorpicker/colorpicker.css" rel="stylesheet" type="text/css" />
-        <link href="assets/jquery-multi-select/multi-select.css"  rel="stylesheet" type="text/css" />
-        <link href="assets/select2/select2.css" rel="stylesheet" type="text/css" />
-        <link href="css/helper.css" rel="stylesheet" type="text/css" />
-        <link href="css/style.css" rel="stylesheet" type="text/css" /><script src="js/modernizr.min.js"></script> </head>
-        <link href="assets/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
-
-
-
-    <body class="fixed-left">
-        
-        <!-- Begin page -->
-        <div id="wrapper">
-        
-            <!-- Top Bar Start -->
-            <div class="topbar">
-                <!-- LOGO -->
-                <div class="topbar-left">
-                    <div class="text-center bg-success">
-                        <a href="index.html" class="logo"><span>Aplikasi Laundry</span></a>
-                    </div>
-                </div>
-                <!-- Button mobile view to collapse sidebar menu -->
-                <div class="navbar navbar-default" role="navigation">
-                    <div class="container bg-success">
-                        <div class="">
-                            <div class="pull-left">
-                                <button class="button-menu-mobile open-left">
-                                    <i class="fa fa-bars"></i>
-                                </button>
-                                <span class="clearfix"></span>
-                            </div>
-                    
-
-                            <ul class="nav navbar-nav navbar-right pull-right">
-
-                                <li class="dropdown">
-                                    <a href="" class="dropdown-toggle profile" data-toggle="dropdown" aria-expanded="true">Selamat Datang, <?php echo $hasil['username']; ?> <img src="images/user.png" alt="user-img" class="img-circle"> </a>
-                                    <ul class="dropdown-menu">
-                              
-                                        <li><a href="logout.php"><i class="md md-settings-power"></i> Logout</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <!--/.nav-collapse -->
-                    </div>
-                </div>
-            </div>
-            <!-- Top Bar End -->
-
-
-            <!-- ========== Left Sidebar Start ========== -->
-
-            <div class="left side-menu">
-                <div class="sidebar-inner slimscrollleft">
-                    <div class="user-details">
-                        <div class="pull-left">
-                            <img src="images/user.png" alt="" class="thumb-md img-circle">
-                        </div>
-                        <div class="user-info">
-                            <div class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><?php echo $hasil['nama']; ?> <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="logout.php"><i class="md md-settings-power"></i>&amp; Logout</a></li>
-                                </ul>
-                            </div>
-                            
-                            <p class="text-muted m-0"><?php echo $hasil['level']; ?></p>
-                        </div>
-                    </div>
-                    <!--- Divider -->
-                    <div id="sidebar-menu">
-                        <ul>
-                            <li>
-                                <a href="?p=home" class="waves-effect"><i class="md md-home"></i><span> Dashboard </span></a>
-                            </li>
-<li>
-                                <a href="?p=tambaht" class="waves-effect"><i class="fa fa-plus"></i><span> Buat Transaksi </span></a>
-                            </li>
-							<li>
-                                <a href="?p=riwayatt" class="waves-effect"><i class="fa fa-clock-o"></i><span> Riwayat Transaksi </span></a>
-                            </li>
-                            <?php if ($hasil['level']!=='Konsumen') { ?>
-														<li>
-                                <a href="?p=tambahko" class="waves-effect"><i class="fa fa-user-plus"></i><span> Tambah Konsumen </span></a>
-                            </li>
-							       <li class="has_sub">
-                                <a href="#" class="waves-effect"><i class="fa fa-list"></i> <span> Data </span> <span class="pull-right"><i class="md md-add"></i></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="?p=barang">Barang</a></li>
-                                    <li><a href="?p=datako">Konsumen</a></li>
-									 <li><a href="?p=datas">Supplier</a></li>
-									  <li><a href="?p=datak">Karyawan</a></li>
-									  <li><a href="?p=jenis">Jenis Laundry</a></li>
-									  <li><a href="?p=beli">Pembelian</a></li>
-									  <li><a href="?p=pakai">Pemakaian</a></li>
-                                </ul>
-                            </li>
-							<?php if ($hasil['level']=='Administrator') { ?>
-							       <li class="has_sub">
-                                <a href="#" class="waves-effect"><i class="fa fa-user"></i> <span> Admin Menu </span> <span class="pull-right"><i class="md md-add"></i></span></a>
-                                <ul class="list-unstyled">
-                                    <li><a href="?p=tambahk">Tambah Karyawan</a></li>
-									 <li><a href="?p=tambahs">Tambah Supplier</a></li>
-									 <li><a href="?p=olahk">Olah Karyawan</a></li>
-									 <li><a href="?p=olahs">Olah Supplier</a></li>
-                                </ul>
-                            </li>
-<?php } ?>
-<?php } ?>
-                        </ul>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-            </div>                    
-            <div class="content-page">
-             
-                <div class="content">
-                    <div class="container">
-                        <div class="row ">
-                            <div class="col-sm-12">
-                                <h4 class="pull-left page-title">Aplikasi Laundry</h4>
-                   
-                            </div>
-                        </div>
-                        <div class='panel panel-border panel-success'>
-                             <?php include "konten.php"; ?>
-                         </div>
-
-
-
-                    </div> 
+$aksi="jenis/proses.php";
+                    $p=isset($_GET['aksi'])?$_GET['aksi']:null;
+                    switch($p){
+default:
+echo "<div class='panel panel-border panel-success'>
+                                    <div class='panel-heading'> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Jenis Laundry</h3> 
+                                    </div>  <div class='panel-body'> 
+									<p align='right'><a class='btn btn-success' href='?p=jenis&aksi=tambah'><i class='icon-plus'></i>Tambah Jenis Laundry</a></p>
+                                   <hr>
+                                <table id='datatable' class='table table-hover'>
+                                    <thead>
+                                        <tr>
+                                            <th><i class='icon-time'></i> Jenis</th>
+                                            <th><i class='icon-signal'></i> Harga/Kg</th>
+										<th><i class='icon-chevron-right'></i> Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>";
+							$i=1;
+							$tp=mysql_query("SELECT * FROM jenis ORDER BY id");
+							while($r=mysql_fetch_array($tp)){
+						    //$hargaa = $r['harga'];
+                             echo"<tr>
+                                    <td>$r[jenis]</td>
+									<td>$r[harga]</td>
+                                    <td><a class='btn btn-success' href='?p=jenis&aksi=edit&id=$r[id]'><i class='icon-edit'></i>Edit</a>
+									 <a class='btn btn-danger' href='$aksi?act=hapus&id=$r[id]'><i class='icon-trash'></i>Hapus</td>
+                                    
+                                </tr>";
+								$i=$i+1;
+							}
                                
-                </div> 
+                                        
+                        echo"</tbody>
+                        </table>
+                                     </div><!-- /.box-body -->
+          </div><!-- /.box -->   ";    
 
-                <footer class="footer text-center">
-                    copyright &copy; 2021 Putri Indriani.
-                </footer>
+break;
+case "edit":
+	$edit = mysql_query("SELECT * FROM jenis WHERE id='$_GET[id]'");
+    $r    = mysql_fetch_array($edit);
+echo "<form method='post' action='jenis/proses.php?act=update' enctype='multipart/form-data'>";
+echo "<div class='panel panel-border panel-success'>
+                                    <div class='panel-heading'> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Edit Jenis Laundry</h3> 
+                                    </div>  <div class='panel-body'> 
+					<input type='hidden' name='id' value='$r[id]'>
+					 <div class='control-group'>
+					   <div class='form-group'>
+                            <label>Jenis</label>
+                            <div class='span9'><input class='form-control' value='$r[jenis]'  type='text' name='jenis' /></div>
+                        </div>						
+					   <div class='form-group'>
+                            <label>Harga/Kg</label>
+                            <div class='span9'><input class='form-control' size='16' type='number' value='$r[harga]' name='harga' /></div>
+                        </div>
+					<Br>
 
-            </div></div>
-    
-        <script>
-            var resizefunc = [];
-        </script>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/waves.js"></script>
-        <script src="js/wow.min.js"></script>
-        <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-        <script src="js/jquery.scrollTo.min.js"></script>
-        <script src="assets/jquery-detectmobile/detect.js"></script>
-        <script src="assets/fastclick/fastclick.js"></script>
-        <script src="assets/jquery-slimscroll/jquery.slimscroll.js"></script>
-        <script src="assets/jquery-blockui/jquery.blockUI.js"></script>
-        <script src="js/jquery.app.js"></script>
-        <script src="assets/tagsinput/jquery.tagsinput.min.js"></script>
-        <script src="assets/toggles/toggles.min.js"></script>
-        <script src="assets/timepicker/bootstrap-timepicker.min.js"></script>
-        <script src="assets/timepicker/bootstrap-datepicker.js"></script>
-        <script type="text/javascript" src="assets/colorpicker/bootstrap-colorpicker.js"></script>
-        <script type="text/javascript" src="assets/jquery-multi-select/jquery.multi-select.js"></script>
-        <script type="text/javascript" src="assets/jquery-multi-select/jquery.quicksearch.js"></script>
-        <script src="assets/bootstrap-inputmask/bootstrap-inputmask.min.js" type="text/javascript"></script>
-        <script type="text/javascript" src="assets/spinner/spinner.min.js"></script>
-        <script src="assets/select2/select2.min.js" type="text/javascript"></script>
+			<input type='submit' class='btn btn-primary' value='Update'> <a class='btn btn-danger' href='?p=harga'>Batal</a>
+		  </form>
+		</div> 
+		                  
+		                  
+                    </div></div>
+					
+		";
+echo "";
+break;
 
-        <script src="assets/datatables/jquery.dataTables.min.js"></script>
-        <script src="assets/datatables/dataTables.bootstrap.js"></script>
+case "tambah":
+echo "<form method='post' action='jenis/proses.php?act=input' enctype='multipart/form-data'>";
+echo '<div class="panel panel-border panel-primary">
+                                    <div class="panel-heading"> 
+                                        <h3 class="panel-title"><i class="fa fa-list"></i> Tambah Barang</h3> 
+                                    </div>  <div class="panel-body"> 
+					 <div class="control-group">
+					   <div class="form-group">
+                            <label>Jenis</label>
+                            <div class="span9"><input class="form-control" placeholder="Masukan Jenis Laundry"  type="text" name="jenis" /></div>
+                        </div>						
+					   <div class="form-group">
+                            <label>Harga/Kg</label>
+                            <div class="span9"><input class="form-control" size="16" type="number" placeholder="Masukan Harga Laundry" name="harga" /></div>
+                        </div>				 
+					<Br>
 
+			<input type="submit" class="btn btn-primary" value="Tambah"> <a class="btn btn-danger" href="?p=jenis">Batal</a>
+		  </form>
+		  <br>
 
-        <script type="text/javascript">
-            $(document).ready(function() {
-                $('#datatable').dataTable();
-            } );
-        </script>
+		</div> 
+		                  
+                   </div></div> ';
+echo "";
+break;
+			}//penutup switch
+?>    
+</body>
 
-
-	
-	</body>
 </html>
