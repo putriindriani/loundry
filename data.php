4d057e8326e20c71087cbaecf5ea4c764358a617
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-27 15:09:59 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,26 +1,31 @@
 <div class='panel panel-border panel-success'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-user'></i> Data Konsumen</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-user'></i> Data Karyawan</h3> 
                                     </div>  <div class='panel-body'> 
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
-											<th><i class='icon-terminal'></i> No</th>
-										<th><i class='icon-terminal'></i> Nama</th>
+										<th><i class='icon-terminal'></i> NIK</th>
+                                            <th><i class='icon-terminal'></i> Nama</th>
+                                            <th><i class='icon-time'></i> Username</th>
 											<th><i class='icon-signal'></i> Alamat</th>
 											<th><i class='icon-signal'></i> Telp</th>
+											<th><i class='icon-signal'></i> Gender</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 							$i=1;
-							$tp=mysql_query("SELECT * FROM konsumen ORDER BY id ");
+							$tp=mysql_query("SELECT * FROM pengguna WHERE level='Karyawan' ORDER BY id ");
 							while($r=mysql_fetch_array($tp)){
 							?>
-							<tr> <td><?php echo $i;?></td>
+							<tr>
+							 <td><?php echo $r['nik'];?></td>
                                     <td><?php echo $r['nama'];?></td>
+									<td><?php echo$r['username'];?></td>
 									<td><?php echo$r['alamat'];?></td>
 									<td><?php echo$r['telp'];?></td>
+									<td><?php echo$r['gender'];?></td>
                                     
 							</tr>
 							<?php $i=$i+1;?>
