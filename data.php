cc688974a1e2a0b839af5cd118d733a7c1eb2061
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-27 15:08:57 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,30 +1,29 @@
 <div class='panel panel-border panel-success'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Pemakaian Barang</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-user'></i> Data Konsumen</h3> 
                                     </div>  <div class='panel-body'> 
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
-										<th><i class='icon-terminal'></i> No</th>
-                                            <th><i class='icon-terminal'></i> Nama Barang</th>
-                                            <th><i class='icon-time'></i> Jumlah</th>
-											<th><i class='icon-signal'></i> Tanggal Pakai</th>
+											<th><i class='icon-terminal'></i> No</th>
+										<th><i class='icon-terminal'></i> Nama</th>
+											<th><i class='icon-signal'></i> Alamat</th>
+											<th><i class='icon-signal'></i> Telp</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 							$i=1;
-							$tp=mysql_query("SELECT * FROM pemakaian ORDER BY id ");
+							$tp=mysql_query("SELECT * FROM konsumen ORDER BY id ");
 							while($r=mysql_fetch_array($tp)){
 							?>
-							<tr>
-							 <td><?php echo $i;?></td>
-                                    <td><?php echo $r['barang'];?></td>
-									<td><?php echo$r['jumlah'];?></td>
-									<td><?php echo TanggalIndo($r['tgl_pakai']);?></td>
+							<tr> <td><?php echo $i;?></td>
+                                    <td><?php echo $r['nama'];?></td>
+									<td><?php echo$r['alamat'];?></td>
+									<td><?php echo$r['telp'];?></td>
                                     
 							</tr>
-						<?php $i=$i+1;?>
+							<?php $i=$i+1;?>
 							<?php } ?>
 							</tbody>
                         </table>
