8216e33f14e3a7995fae52cb5713af8cce333e0c
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-27 15:07:58 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,31 +1,37 @@
 <div class='panel panel-border panel-success'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-user'></i> Data Supplier</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Pembelian Barang</h3> 
                                     </div>  <div class='panel-body'> 
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
 										<th><i class='icon-terminal'></i> No</th>
-										<th><i class='icon-terminal'></i> Nama</th>
-											<th><i class='icon-signal'></i> Alamat</th>
-											<th><i class='icon-signal'></i> Telp</th>
+                                            <th><i class='icon-terminal'></i> Nama Barang</th>
+                                            <th><i class='icon-time'></i> Jumlah</th>
+											<th><i class='icon-signal'></i> Total Harga</th>
+											<th><i class='icon-signal'></i> Supplier</th>
+											<th><i class='icon-signal'></i> Tanggal Pembelian</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 							$i=1;
-							$tp=mysql_query("SELECT * FROM supplier ORDER BY id ");
+							$tp=mysql_query("SELECT * FROM pembelian ORDER BY no ");
 							while($r=mysql_fetch_array($tp)){
 							?>
-							<tr> <td><?php echo $i;?></td>
-                                    <td><?php echo $r['nama'];?></td>
-									<td><?php echo$r['alamat'];?></td>
-									<td><?php echo$r['telp'];?></td>
+							<tr>
+							 <td><?php echo $i;?></td>
+                                    <td><?php echo $r['barang'];?></td>
+									<td><?php echo $r['totali'];?></td>
+									<td><?php echo'Rp.'.number_format($r['totalh'],0,'','.').',-'?></td>
+									<td><?php echo $r['supplier'];?></td>
+									<td><?php echo TanggalIndo($r['tgl']);?></td>
                                     
 							</tr>
 							<?php $i=$i+1;?>
 							<?php } ?>
+								
 							</tbody>
                         </table>
                                      </div><!-- /.box-body -->
-          </div><!-- /.box -->  
+          </div><!-- /.box -->   
