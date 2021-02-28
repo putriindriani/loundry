0a730022dc398134abf02695bf29bf6ef24a1830
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-28 16:19:21 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,105 +1,90 @@
-
+<div class='panel panel-border panel-success'>
+                                    <div class='panel-heading'> 
+                                        <h3 class='panel-title'><i class='fa fa-user-plus'></i> Buat Transaksi</h3> 
+</div>  <div class='panel-body'> 
 <?php
+if(isset($_POST['jenis'])){
+$jeniss	= $_POST['jenis'];
+$query = mysql_query("SELECT * FROM jenis WHERE jenis = '$jeniss'");
+$hasil = mysql_fetch_array($query);
+$harga= $hasil['harga'];
+$berat		= $_POST['berat'];
+$konsumen		= $_POST['konsumen'];
+if ($berat>10){
+$tarif = $berat*$harga*90/100;
+}else{
+$tarif = $berat*$harga;
+}
+$tgl_ambil		= $_POST['tgl_ambil'];
+$timezone = "Asia/Jakarta";
+if(function_exists('date_default_timezone_set')) date_default_timezone_set($timezone);
+$tgl_transaksi=date('Y-m-d');
 
-$aksi="jenis/proses.php";
-                    $p=isset($_GET['aksi'])?$_GET['aksi']:null;
-                    switch($p){
-default:
-echo "<div class='panel panel-border panel-success'>
-                                    <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Jenis Laundry</h3> 
-                                    </div>  <div class='panel-body'> 
-									<p align='right'><a class='btn btn-success' href='?p=jenis&aksi=tambah'><i class='icon-plus'></i>Tambah Jenis Laundry</a></p>
-                                   <hr>
-                                <table id='datatable' class='table table-hover'>
-                                    <thead>
-                                        <tr>
-                                            <th><i class='icon-time'></i> Jenis</th>
-                                            <th><i class='icon-signal'></i> Harga/Kg</th>
-										<th><i class='icon-chevron-right'></i> Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>";
-							$i=1;
-							$tp=mysql_query("SELECT * FROM jenis ORDER BY id");
+	
+	$input = mysql_query("INSERT INTO transaksi VALUES(NULL, '$jeniss','$tarif', '0', '$tgl_transaksi', '$tgl_ambil', '$berat','$usr','$konsumen')") or die(mysql_error());
+	if($input){
+		
+		echo '<div class="alert alert-success alert-dismissable">
+<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><h4><b>
+Transaksi Berhasil!</b></h4>';		
+		echo '
+		<b>Rincian Transaksi</b><br>
+		============================<Br>
+		Konsumen : <b>'.$konsumen.'</b><br>
+		Jenis Laundry : <b>'.$jeniss.'</b><br>
+		Berat : <b>'.$berat.' Kg</b><br>
+		Tarif : <b>Rp. ' . number_format( $tarif, 0 , '' , '.' ) . ',-</b><br>
+		Tanggal Transaksi : <b>'.TanggalIndo($tgl_transaksi).'</b><br>
+		Tanggal Ambil : <b>'.TanggalIndo($tgl_ambil).'</b><br>
+		============================
+		</div>
+		
+		';	
+		
+	}else{
+		
+		echo 'Gagal menambahkan data! ';	
+		echo '<a href="tambah.php">Kembali</a>';	
+		
+	}
+  }
+?>
+									<form method="post">
+																 <div class="form-group">
+                                                <label>Konsumen</label>
+                                                <select  class="form-control" name="konsumen">
+												<?php
+							$tp=mysql_query("SELECT * FROM konsumen ORDER BY id");
 							while($r=mysql_fetch_array($tp)){
-						    //$hargaa = $r['harga'];
-                             echo"<tr>
-                                    <td>$r[jenis]</td>
-									<td>$r[harga]</td>
-                                    <td><a class='btn btn-success' href='?p=jenis&aksi=edit&id=$r[id]'><i class='icon-edit'></i>Edit</a>
-									 <a class='btn btn-danger' href='$aksi?act=hapus&id=$r[id]'><i class='icon-trash'></i>Hapus</td>
-                                    
-                                </tr>";
-								$i=$i+1;
-							}
-                               
-                                        
-                        echo"</tbody>
-                        </table>
-                                     </div><!-- /.box-body -->
-          </div><!-- /.box -->   ";    
-
-break;
-case "edit":
-	$edit = mysql_query("SELECT * FROM jenis WHERE id='$_GET[id]'");
-    $r    = mysql_fetch_array($edit);
-echo "<form method='post' action='jenis/proses.php?act=update' enctype='multipart/form-data'>";
-echo "<div class='panel panel-border panel-success'>
-                                    <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-list'></i> Edit Jenis Laundry</h3> 
-                                    </div>  <div class='panel-body'> 
-					<input type='hidden' name='id' value='$r[id]'>
-					 <div class='control-group'>
-					   <div class='form-group'>
-                            <label>Jenis</label>
-                            <div class='span9'><input class='form-control' value='$r[jenis]'  type='text' name='jenis' /></div>
-                        </div>						
-					   <div class='form-group'>
-                            <label>Harga/Kg</label>
-                            <div class='span9'><input class='form-control' size='16' type='number' value='$r[harga]' name='harga' /></div>
-                        </div>
-					<Br>
-
-			<input type='submit' class='btn btn-primary' value='Update'> <a class='btn btn-danger' href='?p=harga'>Batal</a>
-		  </form>
-		</div> 
-		                  
-		                  
-                    </div></div>
-					
-		";
-echo "";
-break;
-
-case "tambah":
-echo "<form method='post' action='jenis/proses.php?act=input' enctype='multipart/form-data'>";
-echo '<div class="panel panel-border panel-primary">
-                                    <div class="panel-heading"> 
-                                        <h3 class="panel-title"><i class="fa fa-list"></i> Tambah Barang</h3> 
-                                    </div>  <div class="panel-body"> 
-					 <div class="control-group">
-					   <div class="form-group">
-                            <label>Jenis</label>
-                            <div class="span9"><input class="form-control" placeholder="Masukan Jenis Laundry"  type="text" name="jenis" /></div>
-                        </div>						
-					   <div class="form-group">
-                            <label>Harga/Kg</label>
-                            <div class="span9"><input class="form-control" size="16" type="number" placeholder="Masukan Harga Laundry" name="harga" /></div>
-                        </div>				 
-					<Br>
-
-			<input type="submit" class="btn btn-primary" value="Tambah"> <a class="btn btn-danger" href="?p=jenis">Batal</a>
-		  </form>
-		  <br>
-
-		</div> 
-		                  
-                   </div></div> ';
-echo "";
-break;
-			}//penutup switch
-?>    
-</body>
-
-</html>
+							?>
+							<option value="<?php echo $r['nama'];?>"><?php echo $r['nama'];?></option>
+							<?php } ?>
+							</select>
+							 </div>
+																		 <div class="form-group">
+                                                <label>Jenis</label>
+                                                  <select  class="form-control" name="jenis">
+												<?php
+							$tp2=mysql_query("SELECT * FROM jenis ORDER BY id");
+							while($r2=mysql_fetch_array($tp2)){
+							?>
+							<option value="<?php echo $r2['jenis'];?>"><?php echo $r2['jenis'];?></option>
+							<?php } ?>
+							</select>
+											   </div>
+									 <div class="form-group">
+                                                <label>Berat (Dalam Kilogram)</label>
+                                                <input type="number" class="form-control" name="berat" placeholder="Masukan Berat Pakaian" required>
+                                            </div>
+											 <div class="form-group">
+                                                <label>Tanggal Ambil</label>
+                                                <input type="date" class="form-control" name="tgl_ambil" required>
+                                            </div>
+											<pre>
+*Setiap transaksi lebih dari 10kg akan mendapatkan potongan harga 10%</pre>
+<button type="submit" class="btn btn-success waves-effect waves-light">Buat Transaksi</button>
+</form>
+                                     </div>
+									 
+          </div>
+		 
