f78ea042b6fb4a63c47570a3700a2f468ef505e2
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-28 18:02:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,90 +1,193 @@
-<div class='panel panel-border panel-success'>
-                                    <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-user-plus'></i> Buat Transaksi</h3> 
-</div>  <div class='panel-body'> 
-<?php
-if(isset($_POST['jenis'])){
-$jeniss	= $_POST['jenis'];
-$query = mysql_query("SELECT * FROM jenis WHERE jenis = '$jeniss'");
-$hasil = mysql_fetch_array($query);
-$harga= $hasil['harga'];
-$berat		= $_POST['berat'];
-$konsumen		= $_POST['konsumen'];
-if ($berat>10){
-$tarif = $berat*$harga*90/100;
-}else{
-$tarif = $berat*$harga;
+<script type="text/javascript">
+function startCalculate(){
+interval=setInterval("Calculate()",10);
 }
-$tgl_ambil		= $_POST['tgl_ambil'];
-$timezone = "Asia/Jakarta";
-if(function_exists('date_default_timezone_set')) date_default_timezone_set($timezone);
-$tgl_transaksi=date('Y-m-d');
 
-	
-	$input = mysql_query("INSERT INTO transaksi VALUES(NULL, '$jeniss','$tarif', '0', '$tgl_transaksi', '$tgl_ambil', '$berat','$usr','$konsumen')") or die(mysql_error());
-	if($input){
-		
-		echo '<div class="alert alert-success alert-dismissable">
-<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><h4><b>
-Transaksi Berhasil!</b></h4>';		
-		echo '
-		<b>Rincian Transaksi</b><br>
-		============================<Br>
-		Konsumen : <b>'.$konsumen.'</b><br>
-		Jenis Laundry : <b>'.$jeniss.'</b><br>
-		Berat : <b>'.$berat.' Kg</b><br>
-		Tarif : <b>Rp. ' . number_format( $tarif, 0 , '' , '.' ) . ',-</b><br>
-		Tanggal Transaksi : <b>'.TanggalIndo($tgl_transaksi).'</b><br>
-		Tanggal Ambil : <b>'.TanggalIndo($tgl_ambil).'</b><br>
-		============================
-		</div>
-		
-		';	
-		
-	}else{
-		
-		echo 'Gagal menambahkan data! ';	
-		echo '<a href="tambah.php">Kembali</a>';	
-		
-	}
-  }
-?>
-									<form method="post">
-																 <div class="form-group">
-                                                <label>Konsumen</label>
-                                                <select  class="form-control" name="konsumen">
-												<?php
-							$tp=mysql_query("SELECT * FROM konsumen ORDER BY id");
+function Calculate(){
+var a=document.form1.stok.value;
+var b=document.form1.jumlah.value;
+document.form1.stokk.value=(a-b);
+}
+
+function stopCalc(){
+clearInterval(interval);
+}
+
+</script>
+<?php
+
+$aksi="barang/proses.php";
+                    $p=isset($_GET['aksi'])?$_GET['aksi']:null;
+                    switch($p){
+default:
+echo "<div class='panel panel-border panel-success'>
+                                    <div class='panel-heading'> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Barang</h3> 
+                                    </div>  <div class='panel-body'> 
+									<p align='right'><a class='btn btn-success' href='?p=barang&aksi=tambah'><i class='icon-plus'></i>Tambah Barang</a></p>
+                                   <hr>
+                                <table id='datatable' class='table table-hover'>
+                                    <thead>
+                                        <tr>
+                                            <th><i class='icon-terminal'></i> No</th>
+                                            <th><i class='icon-time'></i> Nama</th>
+                                            <th><i class='icon-signal'></i> Stok</th>
+										<th><i class='icon-chevron-right'></i> Tanggal Update Stok</th>
+										<th><i class='icon-chevron-right'></i> Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>";
+							$i=1;
+							$tp=mysql_query("SELECT * FROM barangg ORDER BY id");
 							while($r=mysql_fetch_array($tp)){
-							?>
-							<option value="<?php echo $r['nama'];?>"><?php echo $r['nama'];?></option>
-							<?php } ?>
+						    //$hargaa = $r['harga'];
+                             echo"<tr>
+                                    <td>$i</td>
+									<td>$r[nama]</td>
+									<td>$r[stok]</td>
+                                    <td>".TanggalIndo($r['tgl_update'])."</td>
+                                    <td>
+								     <a class='btn btn-primary' href='?p=barang&aksi=edit&id=$r[id]'><i class='icon-edit'></i>Update Stok</a>
+									 <a class='btn btn-success' href='?p=barang&aksi=pakai&id=$r[id]'><i class='icon-edit'></i>Pakai</a>
+									 <a class='btn btn-danger' href='$aksi?act=hapus&id=$r[id]'><i class='icon-trash'></i>Hapus</td>
+                                    
+                                </tr>";
+								$i=$i+1;
+							}
+                               
+                                        
+                        echo"</tbody>
+                        </table>
+                                     </div><!-- /.box-body -->
+          </div><!-- /.box -->   ";    
+
+break;
+case "edit":
+	$edit = mysql_query("SELECT * FROM barangg WHERE id='$_GET[id]'");
+    $r    = mysql_fetch_array($edit);
+echo "<form method='post' action='barang/proses.php?act=update' enctype='multipart/form-data'>";
+echo "<div class='panel panel-border panel-primary'>
+                                    <div class='panel-heading'> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Tambah Stok Barang</h3> 
+                                    </div>  <div class='panel-body'> 
+					<input type='hidden' name='id' value='$r[id]'>
+					 <div class='control-group'>
+					 					   <div class='form-group'>
+                            <label>Supplier</label>
+                            <div class='span9'><input class='form-control' value='$r[supplier]'  type='text' name='supplier' readonly/></div>
+                        </div>		
+					   <div class='form-group'>
+                            <label>Nama Barang</label>
+                            <div class='span9'><input class='form-control' value='$r[nama]'  type='text' name='nama' readonly/></div>
+                        </div>						
+					   <div class='form-group'>
+                            <label>Harga</label>
+                            <div class='span9'><input class='form-control' size='16' type='number' value='$r[harga]' name='harga' readonly/></div>
+                        </div>
+											   <div class='form-group'>
+                            <label>Jumlah Barang</label>
+                            <div class='span9'><input class='form-control' size='16' type='number' placeholder='Masukan Jumlah Barang' name='jumlah' /></div>
+                        </div>
+					<Br>
+
+			<input type='submit' class='btn btn-primary' value='Update'> <a class='btn btn-danger' href='?p=barang'>Batal</a>
+		  </form>
+		</div> 
+		                  
+		                  
+                    </div></div>
+					
+		";
+echo "";
+break;
+
+case "tambah":
+
+echo "<form method='post' action='barang/proses.php?act=input' enctype='multipart/form-data'>";
+echo '<div class="panel panel-border panel-primary">
+                                    <div class="panel-heading"> 
+                                        <h3 class="panel-title"><i class="fa fa-list"></i> Tambah Barang</h3> 
+                                    </div>  <div class="panel-body"> 
+					 <div class="control-group">
+					 					   <div class="form-group">
+                            <label>Supplier</label>
+							<select class="form-control" name="supplier">
+                            ';
+							$tp=mysql_query("SELECT * FROM supplier ORDER BY id");
+							while($r=mysql_fetch_array($tp)){
+								echo"<option value='$r[nama]'>$r[nama]</option>";
+							}
+							echo'
 							</select>
-							 </div>
-																		 <div class="form-group">
-                                                <label>Jenis</label>
-                                                  <select  class="form-control" name="jenis">
-												<?php
-							$tp2=mysql_query("SELECT * FROM jenis ORDER BY id");
-							while($r2=mysql_fetch_array($tp2)){
-							?>
-							<option value="<?php echo $r2['jenis'];?>"><?php echo $r2['jenis'];?></option>
-							<?php } ?>
-							</select>
-											   </div>
-									 <div class="form-group">
-                                                <label>Berat (Dalam Kilogram)</label>
-                                                <input type="number" class="form-control" name="berat" placeholder="Masukan Berat Pakaian" required>
-                                            </div>
-											 <div class="form-group">
-                                                <label>Tanggal Ambil</label>
-                                                <input type="date" class="form-control" name="tgl_ambil" required>
-                                            </div>
-											<pre>
-*Setiap transaksi lebih dari 10kg akan mendapatkan potongan harga 10%</pre>
-<button type="submit" class="btn btn-success waves-effect waves-light">Buat Transaksi</button>
-</form>
-                                     </div>
-									 
-          </div>
-		 
+							</div>		
+					   <div class="form-group">
+                            <label>Nama Barang</label>
+                            <div class="span9"><input class="form-control" placeholder="Masukan Nama Barang"  type="text" name="nama" /></div>
+                        </div>						
+					   <div class="form-group">
+                            <label>Harga</label>
+                            <div class="span9"><input class="form-control" size="16" type="number" placeholder="Masukan Harga Barang" name="harga" /></div>
+                        </div>
+											   <div class="form-group">
+                            <label>Jumlah Barang</label>
+                            <div class="span9"><input class="form-control" size="16" type="number" placeholder="Masukan Jumlah Barang" name="jumlah" /></div>
+                        </div>				 
+					<Br>
+
+			<input type="submit" class="btn btn-primary" value="Tambah"> <a class="btn btn-danger" href="?p=barang">Batal</a>
+		  </form>
+		  <br>
+
+		</div> 
+		                  
+                   </div></div> ';
+echo "";
+break;
+
+case "pakai":
+	$edit = mysql_query("SELECT * FROM barangg WHERE id='$_GET[id]'");
+    $r    = mysql_fetch_array($edit);
+	
+echo "<form method='post'  name='form1'  action='barang/proses.php?act=pakai' enctype='multipart/form-data'>";
+echo "<div class='panel panel-border panel-primary'>
+                                    <div class='panel-heading'> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Pakai Barang</h3> 
+                                    </div>  <div class='panel-body'> 
+					<input type='hidden' name='id' value='$r[id]'>
+					 <div class='control-group'>	
+					   <div class='form-group'>
+                            <label>Nama Barang</label>
+                            <div class='span9'><input class='form-control' value='$r[nama]'  type='text' name='nama' readonly/></div>
+                        </div>						
+					   <div class='form-group'>
+                            <label>Stok</label>
+                            <div class='span9'><input class='form-control' size='16' type='number' value='$r[stok]' name='stok' readonly/></div>
+                        </div>
+											   <div class='form-group'>
+                            <label>Jumlah Barang</label>
+                            <div class='span9'><input onchange='startCalculate()' class='form-control' size='16' type='number' placeholder='Masukan Jumlah Barang yang akan dipakai' name='jumlah' /></div>
+                        </div>
+											   <div class='form-group'>
+                            <label>Sisa Stok</label>
+                            <div class='span9'><input class='form-control' size='16' type='number' name='stokk'/></div>
+                        </div>
+					<Br>
+
+			<input type='submit' class='btn btn-primary' value='Submit'> <a class='btn btn-danger' href='?p=barang'>Batal</a>
+		  </form>
+		</div> 
+		                  
+		                  
+                    </div></div>
+					
+		";
+echo "";
+break;
+
+			}//penutup switch
+?>    	
+
+
+</body>
+
+</html>
--- proses.php
+++ proses.php
@@ -2,8 +2,9 @@
 $timezone = "Asia/Jakarta";
 if(function_exists('date_default_timezone_set')) date_default_timezone_set($timezone);
 $date=date('Y-m-d');
-			$harga = $_POST['berat'];
-			$harga2 = $harga*5000;
+			$harga = $_POST['harga'];
+			$jumlah = $_POST['jumlah'];
+			$harga2 = $harga*$jumlah;
 			include "../koneksi.php";
                     $p=isset($_GET['act'])?$_GET['act']:null;
                     switch($p){
@@ -11,19 +12,23 @@ $date=date('Y-m-d');
 
                             break;
                         case "input":						
-   mysql_query("INSERT INTO jenis  
-                            VALUES ('','$_POST[jenis]','$_POST[harga]')");
-								   
-   header('location:../index.php?p=jenis');
+   mysql_query("INSERT INTO barangg VALUES ('','$_POST[nama]','$_POST[jumlah]','$date','$_POST[supplier]','$_POST[harga]')");
+   mysql_query("INSERT INTO pembelian VALUES ('','$date','$_POST[jumlah]','$_POST[supplier]','$_POST[nama]','$harga2')");
+		  header('location:../index.php?p=barang');
 	                            break;
                         case "hapus":
-mysql_query("DELETE FROM jenis WHERE id='$_GET[id]'");
+mysql_query("DELETE FROM barangg WHERE id='$_GET[id]'");
   header('location:../index.php?p=barang');
 	                            break;
                         case "update":
-    mysql_query("UPDATE jenis SET jenis='$_POST[jenis]',harga='$_POST[harga]' WHERE id='$_POST[id]'");
-							 
-  header('location:../index.php?p=jenis');  
+    mysql_query("UPDATE barangg SET stok=stok+$_POST[jumlah],tgl_update='$date' WHERE id='$_POST[id]'");
+mysql_query("INSERT INTO pembelian VALUES ('','$date','$_POST[jumlah]','$_POST[supplier]','$_POST[nama]','$harga2')");						 
+  header('location:../index.php?p=barang');  
+  	                            break;
+                        case "pakai":
+    mysql_query("UPDATE barangg SET stok=$_POST[stokk],tgl_update='$date' WHERE id='$_POST[id]'");
+mysql_query("INSERT INTO pemakaian VALUES ('','$date','$_POST[nama]','$_POST[jumlah]')");						 
+  header('location:../index.php?p=barang'); 
 	}
                     ?>
       
