a25e3b400ca6d6f66e585df15a146aa95d0400d9
putriindriani <79629529+putriindriani@users.noreply.github.com>
2021-02-28 16:18:46 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,31 +1,37 @@
 <div class='panel panel-border panel-success'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-user'></i> Data Karyawan</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-clock-o'></i> Riwayat Transaksi</h3> 
                                     </div>  <div class='panel-body'> 
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
-										<th><i class='icon-terminal'></i> NIK</th>
-                                            <th><i class='icon-terminal'></i> Nama</th>
-                                            <th><i class='icon-time'></i> Username</th>
-											<th><i class='icon-signal'></i> Alamat</th>
-											<th><i class='icon-signal'></i> Telp</th>
-											<th><i class='icon-signal'></i> Gender</th>
+										<th><i class='icon-terminal'></i> No</th>
+										<th><i class='icon-signal'></i> Konsumen</th>
+										<th><i class='icon-terminal'></i> Jenis</th>
+											<th><i class='icon-signal'></i> Tarif</th>
+											<th><i class='icon-signal'></i> Berat</th>
+											<th><i class='icon-signal'></i> Tanggal Transaksi</th>
+											<th><i class='icon-signal'></i> Tanggal Ambil</th>
+
+											<th><i class='icon-signal'></i> Kwitansi</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 							$i=1;
-							$tp=mysql_query("SELECT * FROM pengguna WHERE level='Karyawan' ORDER BY id ");
+							$tp=mysql_query("SELECT * FROM transaksi WHERE pengguna='$usr' ORDER BY tgl_transaksi");
 							while($r=mysql_fetch_array($tp)){
 							?>
 							<tr>
-							 <td><?php echo $r['nik'];?></td>
-                                    <td><?php echo $r['nama'];?></td>
-									<td><?php echo$r['username'];?></td>
-									<td><?php echo$r['alamat'];?></td>
-									<td><?php echo$r['telp'];?></td>
-									<td><?php echo$r['gender'];?></td>
+							 <td><?php echo $i;?></td>
+							 
+                                    <td><?php echo $r['konsumen'];?></td>
+                                    <td><?php echo $r['jenis'];?></td>
+									<td><?php echo'Rp.' . number_format( $r['tarif'], 0 , '' , '.' ) . ',-'?></td>
+									<td><?php echo $r['berat']?> Kg</td>
+									<td><?php echo TanggalIndo($r['tgl_transaksi']);?></td>
+									<td><?php echo TanggalIndo($r['tgl_ambil']);?></td>
+									<td><a href="transaksi/kwitansi.php?id=<?php echo $r['id'];?>" target="_blank">Lihat Kwitansi</a></td>
                                     
 							</tr>
 							<?php $i=$i+1;?>
